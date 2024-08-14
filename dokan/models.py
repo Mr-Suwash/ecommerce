bbd0ccd237e96ce75793f54779aac8b1899fccb6
Mr-Suwash <suwashparajuli052@gmail.com>
2024-08-14 19:53:17 +0545

    Day 2

--- dokan/models.py
+++ dokan/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class product(models.Model):
+    Name = models.CharField((""), max_length=50)
+    Price = models.FloatField((""))
+    Discount = models.FloatField((""))
+    Description = models.TextField((""))
+    Image =models.CharField((""), max_length=500)
+    Category = models.CharField((""), max_length=50)
