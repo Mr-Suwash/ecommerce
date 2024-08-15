70e8a13a231ee3d541396c27adf5eda4f297e4af
Mr-Suwash <suwashparajuli052@gmail.com>
2024-08-15 17:40:03 +0545

    Day3

--- dokan/views.py
+++ dokan/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import product
+
+def index(request):
+    product_object= product.objects.all()
+    return render(request, 'dokan/index.html', {'product_object':product_object } )
